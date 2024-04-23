4e19775bd56cca843aca9deca71b99b105e0e86c
Mahesh Awasare <maheshawasare@gmail.com>
2024-04-23 21:59:46 +0530

    Update

--- Server.js
+++ Server.js
@@ -86,5 +86,5 @@ app.post('/login', async (req, res) => {
   
   //Server Start
    app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`SplashCoach backend server is started on port ${port}`)
   })
